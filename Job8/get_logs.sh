84ce2535da7bcd8d6e088315e1376698c6951f8d
“maxime-didomenico” <maxime.di-domenico@laplateforme.io>
2022-10-12 14:52:22 +0200

    grep optimisation

--- Job8/get_logs.sh
+++ Job8/get_logs.sh
@@ -2,8 +2,7 @@
 # 00 * * * * sh /Users/dondada/Desktop/LP/works/shell_exe/Job8/get_logs.sh
 
 Date=`date "+%d-%m-%y-%H:%M"`
-grep "TXkgZGlzY29yZCBEb25EYWRhIzM0MzE" /var/log/system.log  > buff
-grep -v -E "(^#|^$' ')" buff | wc -l > nb_l-$Date
+grep -c "TXkgZGlzY29yZCBEb25EYWRhIzM0MzE" /var/log/system.log  > nb_l-$Date
 tar -zcvf number_connection_$Date.tar.gz nb_l-*
 mv number_connection_*.tar.gz Backup
-rm buff nb_l-*
+rm nb_l-*
