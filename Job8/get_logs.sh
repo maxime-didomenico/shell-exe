2990e774dd0c52ca4738325e5dd2a9a70a01dc97
“maxime-didomenico” <maxime.di-domenico@laplateforme.io>
2022-10-12 11:22:36 +0200

    modification buffer names

--- Job8/get_logs.sh
+++ Job8/get_logs.sh
@@ -2,8 +2,8 @@
 # 00 * * * * sh /Users/dondada/Desktop/LP/works/shell_exe/Job8/get_logs.sh
 
 Date=`date "+%d-%m-%y-%H:%M"`
-last > buff
-grep -v -E "(^#|^$' ')" buff | wc -l > nbl
-tar -zcvf number_connection_$Date.tar.gz nbl
+grep "TXkgZGlzY29yZCBEb25EYWRhIzM0MzE" /var/log/system.log  > buff
+grep -v -E "(^#|^$' ')" buff | wc -l > nb_l-$Date
+tar -zcvf number_connection_$Date.tar.gz nb_l-*
 mv number_connection_*.tar.gz Backup
-rm buff nbl
+rm buff nb_l-*
